cbc1f04cc2cfc7f69e6b8315ce53828d7ffe7f46
Alan Maldonado <business.alanm@gmail.com>
2026-02-22 16:04:26 -0600

    Switch map tiles from CARTO Light to Voyager for softer gray background

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -187,7 +187,7 @@ function MapInner() {
       >
         <MapController mapRef={mapRef} />
         <TileLayer
-          url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+          url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OSM</a> &copy; <a href="https://carto.com/">CARTO</a>'
         />
         <MapEvents
